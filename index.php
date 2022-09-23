5b7fa67d531517b606204815364dc72492c6a01b
PieronchoSISE <piealexandro0202@gmail.com>
2022-09-23 17:20:46 -0500

    Creacion del proyecto

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$db = new PDO('mysql:host=localhost; dbname=financiera', 'sise','sise123');
+$sql="SELECT*FROM cliente";
+$listado = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+print_r($listado);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado - PDO</title>
+</head>
+<body>
+    <h1>1</h1>
+    ...
+    <h6>6</h6>
+    <?php
+        foreach($listado as $cliente):
+    ?>
+
+    <h3><?php echo $cliente['razon_social'] ?></h3>
+    <ul>
+        <li><?php echo $cliente['direccion'] ?></li>
+    </ul>
+
+    <?php
+        endforeach;
+    ?>
+</body>
+</html>
